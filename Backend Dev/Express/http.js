c9cce402182519309f2ce9bffde713f11baa1dc6
googleboy <anandpatel8085@yahoo.com>
2022-10-08 23:45:54 +0530

    08-10-2022

---
+++ Backend Dev/Express/http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(400, { 'content-type': 'text/html' })
+    res.write('<h1>Home page</h1>')
+    res.end();
+})
+
+server.listen(5000);
